3cfa0fe33a280df42e016a35e1b752f3dc081657
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-05-14 09:24:17 +0000

    Create Jest configuration file for consistent test setup

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  verbose: true,
+  setupFiles: ['<rootDir>/tests/setup.ts']
+};
